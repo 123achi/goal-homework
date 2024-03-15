54f7f98ccbbdb19048c0d2a0f0080e746169bbca
123achi <tatazadukeli39@gmail.com>
2024-03-15 16:42:19 +0200

    commit

---
+++ day 6/lesson6.py
@@ -0,0 +1,8 @@
+name = input("whats your name?: ")   
+surname = input("whats your surname?: ")    
+age = int(input("how old are you?: "))
+home = input("where do live?: ")
+job = input("where do you work?: ")
+hobby = input("whats your hobby?: ")
+
+print(name + " " + surname + " " + str(age) + " " + home + " " + job + " " + hobby)
