6ec06e21cfbfc6aaaab89e88c75b4ac42f840b25
123achi <tatazadukeli39@gmail.com>
2024-03-22 14:16:38 +0200

    commit

---
+++ day 7/lesson7.py
@@ -0,0 +1,16 @@
+#მომხმარებელს შემოიტანეთ 3 სხვადასხვა მონაცემტა ტიპი  და გაიგეთ თუ რომელ მონაცემტა ტიპს მიეკუთვნება შემოტანილი ინფორმაცია
+name = input("whats your name?: ")
+age = int(input("how old are you?: "))
+float = float(int(input("enter any number ")))
+print(type(name),type(age),type(float))
+
+
+# შემოატანინეთ 5  სხვადასხვა რიცხვი და ამ შემოტანილ რიცხვებზე მოახდინეთ მათემათიკური ოპერაციები, როგორიცაა +,-,*,/
+num1 = int(input("enter any number: "))
+num2 = int(input("enter any number: "))
+num3 = int(input("enter any number: "))
+num4 = int(input("enter any number: "))
+num5 = int(input("enter any number: ")) 
+
+print(num1 + num2 + num3 + num4 + num5)
+print(num1 - num2 - num3 - num4 - num5)
