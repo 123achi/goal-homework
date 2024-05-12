410cba6b88328df826342656082470f5c6580a80
123achi <tatazadukeli39@gmail.com>
2024-05-12 14:35:39 +0300

    commit

---
+++ day 11/lesson11.py
@@ -0,0 +1,41 @@
+print(10 < 30) 
+print(20 >= 20)
+print(15 == 15)
+print(55 > 25)
+print(50 <= 40)
+print(35 == 35)
+print(45 > 30) 
+print(65 >= 60) 
+print(16 < 45)
+print(70 == 70)
+print(75 <= 30)
+print(80 > 32)
+print(21 < 50)
+print(90 == 90)
+print(95 >= 40)
+print(30 < 65)
+print(15 == 15)
+print
+print
+print       
+
+
+
+
+print(15 > 5 and 15 < 5)
+print(10 > 5 and 10 < 5)
+print(25 > 10 and 25 < 10)
+print(50 > 35 and 50 < 35)
+print(55 > 40 and 55 < 40)
+
+print(35 > 20 or 35 < 20)
+print(45 > 30 or 45 < 30)
+print(75 > 60 or 75 < 60)
+print(80 > 65 or 80 < 65)
+print(90 > 70 or 90 < 70)
+
+
+
+num1 = int(input("enter any number: "))
+num2 = int(input("enter any number: "))
+
---
+++ day 13/lesson13.py
@@ -0,0 +1,129 @@
+for i in range (0, 3, 4):
+    print(0, 3, 4)
+for i in range (0, 5, 6):
+    print(0, 5, 6)
+for i in range (0, 1, 2):
+    print(0, 1, 2)
+for i in range (0, 7, 8):
+    print(0, 7, 8)
+for i in range (0, 9, 10):
+    print(0, 9, 10)
+for i in range (0, 10, 30):
+    print(0, 10, 30)
+for i in range (0, 11, 30):
+    print(0, 11, 30)
+for i in range (0, 12, 30):
+    print(0, 12, 30)
+for i in range (0, 13, 30):
+    print(0, 13, 30)
+for i in range (0, 14, 30):
+    print(0, 14, 30)
+
+
+
+
+
+
+
+
+#While ციკლი
+i = 1
+while i < 10 :
+    print(i)
+    i = i + 1
+
+i = 2
+while i < 10 : 
+    print(i)
+    i = i + 2
+
+i = 3
+while i < 10 :
+    print(i)
+    i = i + 3
+
+i = 4
+while i < 10 :
+    print(i)
+    i = i + 4 
+
+i = 5
+while i < 10 :
+    print(i)
+    i = i + 5
+
+i = 6
+while i < 10 :
+    print(i)
+    i = i + 6
+
+i = 7
+while i < 10 :
+    print(i)
+    i = i + 7
+
+i = 8
+while i < 10 :
+    print(i)
+    i = i + 7
+
+i = 9
+while i < 10 :
+    print(i)
+    i = i + 9
+
+
+        
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+\
+
+
+
+
+
+
+
+
+
+
