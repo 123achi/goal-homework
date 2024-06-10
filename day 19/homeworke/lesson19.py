88b12c84e91f47658a5c1c1b750a1d2cdcfd13fa
123achi <tatazadukeli39@gmail.com>
2024-06-10 17:01:02 +0300

    Adding Files

---
+++ day 19/homeworke/lesson19.py
@@ -0,0 +1,53 @@
+country = [
+    'Georgia' , 'Italy' , 'Slovenia'
+]
+print(country[0])
+
+
+#2
+
+list = [
+    'Georgia' , 10 , True , 19.5 ,False , 45 , 'Hello world' , 20.5 , True , 30 
+]
+
+
+list[1] = 5
+
+list[2] = False
+
+list[5] = 30
+
+list[4] = True
+
+list[9] = 100
+
+
+print(list)
+
+
+
+
+
+#5
+
+games = [
+    'basketball' , 'football' , 'criket' , 'tennis'
+]
+print(games[4])
+
+
+brands = [
+    'nike' , 'addidas' , 'rebook'
+]
+print(brands[0])
+
+
+lists = [
+    'addidas' , 20 , 'Alaska' , 45.5 , True , 100
+]
+
+lists[1] = 40
+
+lists[4] = False
+
+print(lists)
